227eb28a0c30a646426647aa846651cdbc94a2d7
tangigi <tanimanag2003@gmail.com>
2026-03-15 09:32:45 +0530

    code pushed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import Imagecard from './component/Imagecard';
 import './App.css';
 
 function App() {
+  const userData = [
+    { name: "Subhratanu Saha", githubId: "64859956" },
+    { name: "Tanima Nag", githubId: "171805697" },
+    { name: "Soumyadeep M.", githubId: "221869256" },
+    { name: "Sayan Sengupta", githubId: "261204091" },
+    { name: "Debayan Roy", githubId: "172286422" }
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        userData.map((user) => (
+          <Imagecard
+            key={user.githubId}
+            name={user.name}
+            imgLink={`https://avatars.githubusercontent.com/u/${user.githubId}?v=4`}
+          />
+        ))
+      }
     </div>
   );
 }
---
+++ src/component/Imagecard.js
@@ -0,0 +1,12 @@
+
+
+function Imagecard(){
+    console.log("this is imagecard component");
+    return(
+        <div>
+            <h2>Tanima Nag</h2>
+           <img src="https://avatars.githubusercontent.com/u/171805697?s=96&v=4" alt="placeholder"></img>
+        </div>
+    );
+}
+export default Imagecard;
