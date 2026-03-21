9b5ffda3e0817d7505f464de5855007b80ff2cfe
tangigi <tanimanag2003@gmail.com>
2026-03-21 23:45:46 +0530

    feat: functionality changed

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
-import Imagecard from './component/Imagecard';
-import './App.css';
-import { userData } from './constants/constant';
-import { useState } from 'react';
+import { useState } from "react";
+import Imagecard from "./component/Imagecard";
+import "./App.css";
+import { userData } from "./constants/constant";
 
 function App() {
-const [showFlag,setShowFlag]= useState(false);
-const clickHandler=() =>{ 
-  showFlag? setShowFlag(false): setShowFlag(true);
 
-};
+  const [showFlag, setShowFlag] = useState(null);
+
+  const clickHandler = (id) => {
+    if (showFlag === id) {
+      setShowFlag(null);
+    } else {
+      setShowFlag(id);
+    }
+  };
 
   return (
     <div className="App">
 
       {userData.map((user) => (
+
         <div key={user.githubId}>
 
           <Imagecard
             name={user.name}
             imgLink={`https://avatars.githubusercontent.com/u/${user.githubId}`}
           />
-          {/* conditional rendering*/}
-          <h3 style={{cursor:'pointer'}}onClick={clickHandler}>{user.isStudent ? "Student" : "Mentor"}⬇️⬇️⬇️</h3>
-          {showFlag?<p>{user.bioDescription}</p>: null}
+
+          <h3
+            style={{ cursor: "pointer" }}
+            onClick={() => clickHandler(user.githubId)}
+          >
+            {user.isStudent ? "Student" : "Mentor"} ⬇⬇⬇
+          </h3>
+
+          {showFlag === user.githubId && (
+            <p>{user.bioDescription}</p>
+          )}
 
         </div>
+
       ))}
 
     </div>
