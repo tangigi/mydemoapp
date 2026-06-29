fe45b489fbb31085d763b765936987f2e2370652
tangigi <tanimanag2003@gmail.com>
2026-06-30 00:06:12 +0530

    feature changed

---
+++ src/constants/constant.js
@@ -0,0 +1,7 @@
+export const userData = [
+    { name: "Subhratanu Saha", githubId: "64859956", isStudent: false, bioDescription: "Subhratanu having 4 Years of IT Experience and having 7 Years of Teaching experience." },
+    { name: "Tanima Nag", githubId: "171805697", isStudent: true, bioDescription: "Tanima is a passionate student with a keen interest in web development." },
+    { name: "Soumyadeep M.", githubId: "221869256", isStudent: true, bioDescription: "Soumyadeep is an enthusiastic learner and aspiring developer." },
+    { name: "Sayan Sengupta", githubId: "261204091", isStudent: true, bioDescription: "Sayan is a dedicated student with a strong foundation in computer science." },
+    { name: "Debayan Roy", githubId: "172286422", isStudent: true, bioDescription: "Debayan is a motivated student looking to enhance his programming skills." }
+  ];
